04c4d269bacba59562c418d0e29d6c32ef62b75e
Matias <mparodi@evosoftuy.com>
2018-12-02 20:16:18 -0300

    Primer commit

---
+++ server/classes/usuarios.js
@@ -0,0 +1,47 @@
+
+
+class Usuarios {
+    constructor(){
+        this.personas = [];
+    }
+
+
+    agregarPersona(id, nombre, sala){
+        let persona = {id, nombre, sala}
+        this.personas.push(persona);
+        return this.personas;
+    }
+
+    getPersona(id){
+        
+        let persona = this.personas.find(per=> per.id == id);
+
+        return persona;
+    }
+
+    getPersonas(){
+        return this.personas;
+    }
+
+    getPersonasPorSala(sala){
+        let personasEnSala = this.personas.filter(persona=>  persona.sala === sala);
+        return personasEnSala;
+    }
+
+    borrarPersona(id){
+        let personaBorrada = this.getPersona(id);
+
+        this.personas = this.personas.filter(per=>{
+            return per.id != id
+        });
+
+        return personaBorrada;
+    }
+
+}
+
+
+
+module.exports = {
+    Usuarios
+}
