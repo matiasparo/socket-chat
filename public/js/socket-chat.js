d780633bdb0f85fc50ed4b59e5e2696f655df3b2
Matias <mparodi@evosoftuy.com>
2018-12-02 21:39:26 -0300

    Chat Finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,25 +1,26 @@
 var socket = io();
+
 var params = new URLSearchParams(window.location.search);
 
-
-if(!params.has('nombre') || !params.has('sala')){
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error("El nombre es necesario y sala son necesarios");
-
+    throw new Error('El nombre y sala son necesarios');
 }
-
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(resp){
-        console.log("usuarios conectados", resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -30,30 +31,28 @@ socket.on('disconnect', function() {
 });
 
 
-
-
 // Enviar información
-socket.emit('crearMensaje', {
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensaje(mensaje, false);
 });
 
-//Escuchar cambos cundo un usuario entra y sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
-socket.on('mensajePrivado', function(mensaje){
-    console.log('Mensaje Privado', mensaje);
-})
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
